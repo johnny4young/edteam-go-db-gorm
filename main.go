127472e497244cfe908f5c4a74497efe3b562f65
johnny4young <asesordeprogramacion@gmail.com>
2021-09-22 22:10:38 -0500

    added read crud

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/johnny4young/edteam-go-db-gorm/model"
 	"github.com/johnny4young/edteam-go-db-gorm/storage"
 )
@@ -16,17 +18,25 @@ func main() {
 	)
 
 	// Create
-	product1 := model.Product{
-		Name:  "Go's Course",
-		Price: 120,
+	// product1 := model.Product{
+	// 	Name:  "Go's Course",
+	// 	Price: 120,
+	// }
+
+	// obs := "testing with GO"
+	// product2 := model.Product{
+	// 	Name:         "Go's Testing",
+	// 	Price:        80,
+	// 	Observations: &obs,
+	// }
+	// storage.DB().Create(&product1)
+	// storage.DB().Create(&product2)
+
+	// READ
+	products := make([]model.Product, 0)
+	storage.DB().Find(&products)
+	for _, product := range products {
+		fmt.Printf("%d - %s\n", product.ID, product.Name)
 	}
 
-	obs := "testing with GO"
-	product2 := model.Product{
-		Name:         "Go's Testing",
-		Price:        80,
-		Observations: &obs,
-	}
-	storage.DB().Create(&product1)
-	storage.DB().Create(&product2)
 }
